26f4325d15b3af798d8e57dfeacb04231024df65
Anthony Chen <50970130+achen318@users.noreply.github.com>
2025-08-11 01:40:49 -0400

    Allow CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routers import rooms, users
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(rooms.router)
 app.include_router(users.router)
