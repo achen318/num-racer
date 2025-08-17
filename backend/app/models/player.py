626832307ca0c4b337c46a82c7ff58d4891976f3
Anthony Chen <50970130+achen318@users.noreply.github.com>
2025-08-17 17:03:12 -0400

    Add player model and tests

---
+++ backend/app/models/player.py
@@ -0,0 +1,45 @@
+"""
+Represents players in a match.
+"""
+
+from app.models.problem import Problem
+from pydantic import BaseModel
+
+
+class Player(BaseModel):
+    """
+    Represents a player in a match.
+    """
+
+    name: str
+    score: int = 0
+    current_problem: Problem | None = None
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.score}) - {self.current_problem})"
+
+    def check(self, answer: int) -> bool:
+        """
+        Checks if the given answer is correct for the current problem. Grants
+        a point to the player if the answer is correct.
+
+        Args:
+            answer: The player's answer to the current problem.
+
+        Returns:
+            bool: True if the answer is correct, False otherwise.
+        """
+        if self.current_problem and answer == self.current_problem.result:
+            self.score += 1
+            return True
+
+        return False
+
+    def assign_problem(self, problem: Problem) -> None:
+        """
+        Assigns a new problem to the player.
+
+        Args:
+            problem: The problem to assign to the player.
+        """
+        self.current_problem = problem
---
+++ backend/tests/models/test_player.py
@@ -0,0 +1,68 @@
+import pytest
+from app.models.player import Player
+from app.models.problem import Operation, Problem
+
+
+@pytest.fixture
+def player():
+    return Player(name="Test Player")
+
+
+@pytest.fixture
+def problem_1():
+    return Problem(num1=1, num2=1, operation=Operation.ADD, result=2)
+
+
+@pytest.fixture
+def problem_2():
+    return Problem(num1=2, num2=2, operation=Operation.ADD, result=4)
+
+
+def test_assign_problem(player: Player, problem_1: Problem):
+    assert player.current_problem is None
+
+    player.assign_problem(problem_1)
+    assert player.current_problem == problem_1
+
+
+def test_assign_problems(player: Player, problem_1: Problem, problem_2: Problem):
+    assert player.current_problem is None
+
+    player.assign_problem(problem_1)
+    assert player.current_problem == problem_1
+
+    player.assign_problem(problem_2)
+    assert player.current_problem == problem_2
+
+
+def test_check_incorrect(player: Player, problem_1: Problem):
+    assert player.score == 0
+
+    player.assign_problem(problem_1)
+    assert not player.check(3)
+    assert player.score == 0
+
+
+def test_check_correct(player: Player, problem_1: Problem):
+    assert player.score == 0
+
+    player.assign_problem(problem_1)
+    assert player.check(2)
+    assert player.score == 1
+
+
+def test_check_multiple(player: Player, problem_1: Problem):
+    assert player.score == 0
+
+    player.assign_problem(problem_1)
+    assert not player.check(3)
+    assert player.score == 0
+
+    assert player.check(2)
+    assert player.score == 1
+
+    assert not player.check(1)
+    assert player.score == 1
+
+    assert player.check(2)
+    assert player.score == 2
